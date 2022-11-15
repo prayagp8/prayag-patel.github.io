973cf92119940fee90157fc519dbcd63b3a0a771
Prayag Patel <prayagp8@gmail.com>
2022-11-15 18:34:16 +0530

    function resume add

--- js/script.js
+++ js/script.js
@@ -1,26 +1,34 @@
 let menu = document.querySelector('#menu-bars');
 let header = document.querySelector('header');
 
-menu.onclick = () =>{
+menu.onclick = () => {
     menu.classList.toggle('fa-times');
     header.classList.toggle('active');
 }
 
-window.onscroll = () =>{
+window.onscroll = () => {
     menu.classList.remove('fa-times');
     header.classList.remove('active');
 }
 
-document.querySelectorAll('a').forEach(links =>{
+document.querySelectorAll('a').forEach(links => {
 
-    links.onmouseenter = () =>{
+    links.onmouseenter = () => {
         cursor1.classList.add('active');
         cursor2.classList.add('active');
     }
 
-    links.onmouseleave = () =>{
+    links.onmouseleave = () => {
         cursor1.classList.remove('active');
         cursor2.classList.remove('active');
     }
 
+});
+
+
+
+
+document.getElementById("myyfun").addEventListener("click", function () {
+    window.location.href =
+        "https://drive.google.com/file/d/10qJBjQqiEWfQo5XoC7RfgjcwoYVH_JyR/view?usp=sharing";
 });
